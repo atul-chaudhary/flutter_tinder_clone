cbf78e82da78f947e50dbd7a1615655cb7f3f225
atulc <chaudharyatul444@gmail.com>
2019-12-26 14:42:40 +0530

    complete main page

---
+++ lib/login_number.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginNumber extends StatefulWidget {
+  @override
+  _LoginNumberState createState() => _LoginNumberState();
+}
+
+class _LoginNumberState extends State<LoginNumber> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+    );
+  }
+}
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,20 +10,159 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      width: double.infinity,
-      height: double.infinity,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromRGBO(253, 41, 123, 1),
-            Color.fromRGBO(255, 88, 100, 1),
-            Color.fromRGBO(255, 101, 91, 1)
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(253, 41, 123, 1),
+              Color.fromRGBO(255, 88, 100, 1),
+              Color.fromRGBO(255, 101, 91, 1)
+            ],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 180, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    "assets/tinder_icon.png",
+                    width: 40,
+                    height: 40,
+                    fit: BoxFit.fill,
+                  ),
+                  SizedBox(
+                    width: 3,
+                  ),
+                  Text(
+                    "tinder",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                        letterSpacing: -2.0),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Text(
+                        "By tapping Log in, you agree with our",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "Terms of service",
+                            style: TextStyle(
+                                color: Colors.white,
+                                decoration: TextDecoration.underline,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            " and ",
+                            style: TextStyle(
+                                color: Colors.white, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "Privacy Policy",
+                            style: TextStyle(
+                                color: Colors.white,
+                                decoration: TextDecoration.underline,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        width: double.infinity,
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
+                        margin: EdgeInsets.fromLTRB(24, 20, 24, 10),
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                                colors: [Colors.indigo[800], Colors.blue],
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight),
+                            borderRadius: BorderRadius.circular(26)),
+                        child: Text(
+                          "LOG IN WITH FACEBOOK",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16),
+                        ),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
+                        margin: EdgeInsets.fromLTRB(24, 0, 24, 10),
+                        decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            border: Border.all(color: Colors.white, width: 2),
+                            borderRadius: BorderRadius.circular(26)),
+                        child: Text(
+                          "LOG IN WITH PHONE NUMBER",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        "Trouble logging in?",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "We don't post anything on Facebook",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 3,
+                          ),
+                          Icon(Icons.keyboard_arrow_down,color: Colors.white,),
+                        ],
+                      ),
+                      SizedBox(height: 8,),
+                    ],
+                  ),
+                ),
+              ),
+            )
           ],
-          begin: Alignment.bottomLeft,
-          end: Alignment.topRight,
         ),
       ),
-    ));
+    );
   }
 }
